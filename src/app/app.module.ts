0cc370093740673648aa9c2fae3ec030340cc24e
TeelaPat26 <teelapat.pra@gmail.com>
2022-09-18 16:48:50 +0700

    Create login and register page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import {AngularFireModule} from '@angular/fire/compat';
 import { LoginComponent } from './component/login/login.component';
 import { RegisterComponent } from './component/register/register.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component'
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -22,10 +23,8 @@ import { DashboardComponent } from './component/dashboard/dashboard.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/auth.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private fireauth: AngularFireAuth,
+    private router: Router
+    ) { }
+
+    // Login method
+    login(email: string, password: string) {
+      this.fireauth.signInWithEmailAndPassword(email, password).then( () => {
+        localStorage.setItem('token', 'true');
+      }, err => {
+        alert('Something went wrong');
+        this.router.navigate(['/login']);
+      })
+    }
+
+    // register
+    register(email: string, password: string) {
+      this.fireauth.createUserWithEmailAndPassword(email, password).then( () => {
+        alert('Register successful');
+        this.router.navigate(['/login']);
+      }, err => {
+        alert(err.message);
+        this.router.navigate(['/login'])
+      })
+    }
+
+    // sign out
+    logout(){
+      this.fireauth.signOut().then( () => {
+        localStorage.removeItem('token');
+        this.router.navigate(['/login'])
+      }, err => {
+        alert(err.message)
+      })
+    }
+
+}
