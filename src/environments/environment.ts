21fab407d52131eb506056e44018356b0779d4b9
TeelaPat26 <teelapat.pra@gmail.com>
2022-09-18 15:55:49 +0700

    config connection to firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'angular-firebase-authen-d730e',
+    appId: '1:838925285965:web:db8e64401945c2f7defa04',
+    storageBucket: 'angular-firebase-authen-d730e.appspot.com',
+    apiKey: 'AIzaSyDr4vsbYgkcRHcKBN-0qaFvPDx5mbDXXp8',
+    authDomain: 'angular-firebase-authen-d730e.firebaseapp.com',
+    messagingSenderId: '838925285965',
+    measurementId: 'G-L1P6RGWR6Y',
+  },
   production: false
 };
 
